0290925da80d0fbd470d89cf7c42b0cc4ebef269
BDFD <bdfd2005@gmail.com>
2021-10-10 15:02:37 -0400

    ✨ feat(Section Page): Redesign Main Page
    Put Info into Section Page

--- public/javascripts/protect.js
+++ public/javascripts/protect.js
@@ -2,35 +2,29 @@ document.onkeydown = function () {
 	var e = window.event || arguments[0];
 	if (e.keyCode == 123) {
 		alert(
-			'请尊重劳动成果！请联系我bdfd2005@gmail.com \n 请返回 http://www./ 谢谢合作!'
+			'请尊重劳动成果！请联系我davidchen014@gmail.com \n 请返回 https://dc-personal-portfolio.herokuapp.com/ 谢谢合作!'
 		);
 		return false;
 	} else if (e.ctrlKey && e.shiftKey && e.keyCode == 73) {
 		alert(
-			'请尊重劳动成果！请联系我bdfd2005@gmail.com\n请返回 http://www./ 谢谢合作!'
+			'请尊重劳动成果！请联系我davidchen014@gmail.com \n请返回 https://dc-personal-portfolio.herokuapp.com/ 谢谢合作!'
 		);
 		return false;
 	} else if (e.ctrlKey && e.keyCode == 85) {
 		alert(
-			'请尊重劳动成果！请联系我bdfd2005@gmail.com\n请返回 http://www./ 谢谢合作!'
+			'请尊重劳动成果！请联系我davidchen014@gmail.com \n请返回 https://dc-personal-portfolio.herokuapp.com/ 谢谢合作!'
 		);
 		return false;
 	} else if (e.ctrlKey && e.keyCode == 83) {
 		alert(
-			'请尊重劳动成果！请联系我bdfd2005@gmail.com\n请返回 http://www./ 谢谢合作!'
+			'请尊重劳动成果！请联系我davidchen014@gmail.com \n请返回 https://dc-personal-portfolio.herokuapp.com/ 谢谢合作!'
 		);
 		return false;
 	}
 };
 document.oncontextmenu = function () {
 	alert(
-		'请尊重劳动成果！请联系我bdfd2005@gmail.com\n请返回 http://www./ 谢谢合作!'
+		'请尊重劳动成果！请联系我davidchen014@gmail.com \n请返回 https://dc-personal-portfolio.herokuapp.com/ 谢谢合作!'
 	);
 	return false;
 };
-
-// var host2 = document.domain;
-// var sear = new RegExp('kcresume');
-// if (!sear.test(host2)) {
-// 	window.location.href = 'http://www.kcresume.tk/';
-// }
--- routes/index.js
+++ routes/index.js
@@ -8,35 +8,35 @@ var router = express.Router();
 // 	res.render('Index/Login', { layout: 'page_layout/Login_layout' });
 // });
 router.get('/', function (req, res, next) {
-	res.render('Index/home', { layout: 'page_layout/Resume_layout' });
+	res.render('Index/01home', { layout: 'page_layout/Resume_layout' });
 });
 
 router.get('/home', function (req, res, next) {
-	res.render('Index/home', { layout: 'page_layout/Resume_layout' });
-});
-
-router.get('/work_exp', function (req, res, next) {
-	res.render('Index/work_exp', { layout: 'page_layout/Resume_layout' });
+	res.render('Index/01home', { layout: 'page_layout/Resume_layout' });
 });
 
 router.get('/skill_list', function (req, res, next) {
-	res.render('Index/skill_list', { layout: 'page_layout/Resume_layout' });
+	res.render('Index/02skill_list', { layout: 'page_layout/Resume_layout' });
+});
+
+router.get('/work_exp', function (req, res, next) {
+	res.render('Index/03work_exp', { layout: 'page_layout/Resume_layout' });
 });
 
 router.get('/project', function (req, res, next) {
-	res.render('Index/project', { layout: 'page_layout/Resume_layout' });
-});
-
-router.get('/gallery', function (req, res, next) {
-	res.render('Index/gallery', { layout: 'page_layout/Resume_layout' });
+	res.render('Index/04project', { layout: 'page_layout/Resume_layout' });
 });
 
 router.get('/demo', function (req, res, next) {
-	res.render('Index/demo', { layout: 'page_layout/Resume_layout' });
+	res.render('Index/05demo', { layout: 'page_layout/Resume_layout' });
+});
+
+router.get('/gallery', function (req, res, next) {
+	res.render('Index/06gallery', { layout: 'page_layout/Resume_layout' });
 });
 
 router.get('/contact', function (req, res, next) {
-	res.render('Index/contact', { layout: 'page_layout/Resume_layout' });
+	res.render('Index/07contact', { layout: 'page_layout/Resume_layout' });
 });
 
 module.exports = router;
