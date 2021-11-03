d005c8530a3b10932552c25dc0cbf7924cc5e33e
BDFD <bdfd2005@gmail.com>
2021-11-02 21:59:10 -0400

    📃 docs(Update Info): Update Info
    Home Page and Skill Set Page

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: BDFD
+ * @Date: 2021-10-10 13:47:41
+ * @LastEditTime: 2021-11-02 21:02:24
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \Project1.1_David-Online-Portfolio\routes\index.js
+ */
 var express = require('express');
 var router = express.Router();
 
@@ -17,6 +25,11 @@ router.get('/home', function (req, res, next) {
 
 router.get('/skill_list', function (req, res, next) {
 	res.render('Index/02skill_list', { layout: 'page_layout/Resume_layout' });
+});
+router.get('/certificate', function (req, res, next) {
+	res.render('Index/02_1certificate_list', {
+		layout: 'page_layout/Resume_layout',
+	});
 });
 
 router.get('/work_exp', function (req, res, next) {
